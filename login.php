d61785a3dd7eefffadbd0e215acc4f8348fcad83
ratuhusna <ratuhusna09@gmail.com>
2015-11-03 09:27:12 +0700

    login.php

---
+++ login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>ARB Express</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	
+	<script src="js/jquery-1.6.2.min.js" type="text/javascript" charset="utf-8"></script>
+	<!--[if IE 6]>
+		<script src="js/DD_belatedPNG-min.js" type="text/javascript" charset="utf-8"></script>
+	<![endif]-->
+	<script src="js/jquery.jcarousel.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/functions.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+	<!-- Begin Wrapper -->
+	<div id="wrapper">
+		<!-- Begin Header -->
+		<!-- <div id="header" class="shell">
+			<h1 id="logo"><a class="notext" href="#" title="PC Store">PC Store</a></h1>
+		</div> -->
+		<div id="header">
+			<div class="shell">
+				<p><h1><b>ARB Express</b></h1></p>
+			</div>
+		</div>
+		<!-- End Header -->
+		<!-- Begin Navigation -->
+		<div id="navigation">
+			<div class="shell">
+				<ul>
+					<li><a href="home.html" title="Home">Home</a></li>
+					<li><a href="login.php" title="Log In">Log In</a></li>
+					<li><a href="#" title="Services">Services</a></li>
+					<li><a href="#" title="Contact">Contact</a></li>
+					<li><a href="#" title="About">About</a></li>
+				</ul>
+				<div class="cl">&nbsp;</div>
+			</div>
+		</div>
+		<!-- End Navigation -->
+		<!-- Begin Main -->
+		<div id="main">
+			<!-- Begin Inner -->
+			<div class="inner">
+				<div class="shell">
+					<!-- Begin Left Sidebar -->
+					<div id="left-sidebar" class="sidebar">
+						<ul>
+							<li class="widget">
+								<h2>Profile</h2>
+								<div class="widget-entry">
+									<ul>
+										<li><a href="#" title="Edit Profile"><span>Edit Profile</span></a></li>
+										<li><a href="#" title="My Point"><span>My Point</span></a></li>
+										<li><a href="#" title="Notifications"><span>Notifications</span></a></li>
+										<li class="last"><a href="#" title="Logout"><span>Logout</span></a></li>
+									</ul>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<!-- End Sidebar -->
+					<!-- Begin Content -->
+					<div id="content">
+						<!-- Begin Post --><!-- End Post -->
+						<!-- Begin Products -->
+					    <div id="products">
+					    <h2>LOGIN</h2>
+                        <form action="" method="post">
+			<div>
+				<input type="text" name="user" placeholder="Username" class="lg"/>
+			</div>
+			<div style="margin-top:10px">
+				<input type="password" name="pass" placeholder="Password" class="lg"/>
+			</div>
+			<div style="margin-top:10px">
+				<input type="submit" name="login" value="Login" class="btn"/>
+			</div>
+		</form>
+						  <div class="cl">&nbsp;</div>
+						</div>
+						<!-- End Products -->
+				  </div>
+					<!-- End Content -->
+					<div class="cl">&nbsp;</div>
+				</div>
+			</div>
+			<!-- End Inner -->
+		</div>
+		<!-- End Main -->
+		<!-- Begin Footer -->
+		<div id="footer">
+			<div class="shell">
+				<p>2015 &copy; ARB Express | All Rights Reserved</p>
+				<div class="cl">&nbsp;</div>
+			</div>
+		</div>
+		<!-- End Footer -->
+	</div>
+	<!-- End Wrapper -->
+</body>
+</html>
