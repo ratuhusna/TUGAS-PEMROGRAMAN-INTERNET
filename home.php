9f4812a3a26f9da75e0a083eb5b7d11dd21c59e7
ratuhusna <ratuhusna09@gmail.com>
2015-11-03 09:25:43 +0700

    home.html

---
+++ home.php
@@ -0,0 +1,115 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>ARB Express</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	
+	<script src="js/jquery-1.6.2.min.js" type="text/javascript" charset="utf-8"></script>
+	<!--[if IE 6]>
+		<script src="js/DD_belatedPNG-min.js" type="text/javascript" charset="utf-8"></script>
+	<![endif]-->
+	<script src="js/jquery.jcarousel.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/functions.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+	<!-- Begin Wrapper -->
+	<div id="wrapper">
+		<!-- Begin Header -->
+		<!-- <div id="header" class="shell">
+			<h1 id="logo"><a class="notext" href="#" title="PC Store">PC Store</a></h1>
+		</div> -->
+		<div id="header">
+			<div class="shell">
+				<p><h1><b>ARB Express</b></h1></p>
+			</div>
+		</div>
+		<!-- End Header -->
+		<!-- Begin Navigation -->
+		<div id="navigation">
+			<div class="shell">
+				<ul>
+					<li><a href="home.html" title="Home">Home</a></li>
+					<li><a href="login.php" title="Log In">Log In</a></li>
+					<li><a href="#" title="Services">Services</a></li>
+					<li><a href="#" title="Contact">Contact</a></li>
+					<li><a href="#" title="About">About</a></li>
+				</ul>
+				<div class="cl">&nbsp;</div>
+			</div>
+		</div>
+		<!-- End Navigation -->
+		<!-- Begin Main -->
+		<div id="main">
+			<!-- Begin Inner -->
+			<div class="inner">
+				<div class="shell">
+					<!-- Begin Left Sidebar -->
+					<div id="left-sidebar" class="sidebar">
+						<ul>
+							<li class="widget">
+								<h2>Profile</h2>
+								<div class="widget-entry">
+									<ul>
+										<li><a href="#" title="Edit Profile"><span>Edit Profile</span></a></li>
+										<li><a href="#" title="My Point"><span>My Point</span></a></li>
+										<li><a href="#" title="Notifications"><span>Notifications</span></a></li>
+										<li class="last"><a href="#" title="Logout"><span>Logout</span></a></li>
+									</ul>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<!-- End Sidebar -->
+					<!-- Begin Content -->
+					<div id="content">
+						<!-- Begin Post -->
+						<div class="post">
+							<h2>Welcome to ARB Express<span class="title-bottom">&nbsp;</span></h2>
+							<p>Selamat datang di ARB Express. disini kalian bisa melihat vtarif pengiriman paket dengan jenis yang berbeda-beda... so enjoy</p>
+						</div>
+						<!-- End Post -->
+						<!-- Begin Products -->
+						<div id="products">
+							<h2>Jenis Paket<span class="title-bottom">&nbsp;</span></h2>
+							<div class="product">
+								<a href="#" title="Paket A">
+									<span class="title">Paket A</span>
+									<img src="css/images/question-mark.jpg" alt="Product Image 1" />
+								</a>
+							</div>
+							<div class="product">
+								<a href="#" title="Paket B">
+									<span class="title">Paket B</span>
+									<img src="css/images/question-mark.jpg" alt="Product Image 2" />
+								</a>
+							</div>
+							<div class="product last">
+								<a href="#" title="Paket C">
+									<span class="title">Paket C</span>
+									<img src="css/images/question-mark.jpg" alt="Product Image 3" />
+								</a>
+							</div>
+							<div class="cl">&nbsp;</div>
+						</div>
+						<!-- End Products -->
+					</div>
+					<!-- End Content -->
+					<div class="cl">&nbsp;</div>
+				</div>
+			</div>
+			<!-- End Inner -->
+		</div>
+		<!-- End Main -->
+		<!-- Begin Footer -->
+		<div id="footer">
+			<div class="shell">
+				<p>2015 &copy; ARB Express | All Rights Reserved</p>
+				<div class="cl">&nbsp;</div>
+			</div>
+		</div>
+		<!-- End Footer -->
+	</div>
+	<!-- End Wrapper -->
+</body>
+</html>
